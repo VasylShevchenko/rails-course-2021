e88f88bc6feef4d07ac603573b3fd7bb761ae3e5
Vasyl Shevchenko <vasya.shevchenko.2011@gmail.com>
2021-04-13 17:50:00 +0300

    User add card number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,10 +36,6 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-
-
-    debugger
-
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: "User was successfully updated." }
@@ -68,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :password, :age)
+      params.require(:user).permit(:email, :password, :age, :card_number)
     end
 end
